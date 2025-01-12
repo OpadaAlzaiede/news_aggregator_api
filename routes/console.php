7516886941f16b7419fa4281f78478370c00be80
obada <opadaalziede@gmail.com>
2025-01-12 21:03:53 +0300

    wip: schedule commands to run every mondau

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Console\Commands\SyncBBCApiArticlesCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '00:00');
+Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '01:00');
+Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '02:00');
