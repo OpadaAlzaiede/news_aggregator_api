d2436f09e78870559e4e81c9ee4d5d61dd544f50
obada <opadaalziede@gmail.com>
2025-01-13 01:11:59 +0300

    fix: calling the same sync command in console.php

--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,11 @@
 <?php
 
 use App\Console\Commands\SyncBBCApiArticlesCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\SyncNewsApiArticlesCommand;
+use App\Console\Commands\SyncNYTimesApiArticlesCommand;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(SyncBBCApiArticlesCommand::class)->everyMinute();
-Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '01:00');
-Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '02:00');
+Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '00:00');
+Schedule::command(SyncNewsApiArticlesCommand::class)->weeklyOn(1, '01:00');
+Schedule::command(SyncNYTimesApiArticlesCommand::class)->weeklyOn(1, '02:00');
