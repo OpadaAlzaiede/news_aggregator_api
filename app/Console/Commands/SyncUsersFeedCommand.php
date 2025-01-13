e263e8b935b90581db786e6d0d8e9147d28648bd
obada <opadaalziede@gmail.com>
2025-01-13 13:45:45 +0300

    wip: add SyncUsersFeedCommand to sync users feed after news syncing

---
+++ app/Console/Commands/SyncUsersFeedCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\HasPreferencesEnum;
+use App\Jobs\SyncUserFeedJob;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SyncUsersFeedCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync-users-feed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command syncs all users feed.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::query()
+            ->where('has_preferences', HasPreferencesEnum::YES->value)
+            ->chunk(100, function($users) {
+                foreach($users as $user) {
+
+                    SyncUserFeedJob::dispatch($user);
+                }
+            });
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\SyncUsersFeedCommand;
 use App\Console\Commands\SyncBBCApiArticlesCommand;
 use App\Console\Commands\SyncNewsApiArticlesCommand;
 use App\Console\Commands\SyncNYTimesApiArticlesCommand;
-use Illuminate\Support\Facades\Schedule;
 
 
 Schedule::command(SyncBBCApiArticlesCommand::class)->weeklyOn(1, '00:00');
 Schedule::command(SyncNewsApiArticlesCommand::class)->weeklyOn(1, '01:00');
 Schedule::command(SyncNYTimesApiArticlesCommand::class)->weeklyOn(1, '02:00');
+Schedule::command(SyncUsersFeedCommand::class)->weeklyOn(1, '03:00');
