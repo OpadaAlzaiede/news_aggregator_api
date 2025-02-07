3d39e134c5aadcde3006a8cf030f5010773b5be6
obada <opadaalziede@gmail.com>
2025-02-07 23:32:06 +0300

    wip: remove api.php file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,15 @@
 <?php
 
-use App\Http\Middleware\RequireJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function() {
